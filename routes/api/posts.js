0b1d179d952387a95dda778dd1f3d6b0887d6ee8
stevechanvii <dche0007@student.monash.edu>
2020-03-06 00:42:29 +1100

    setup RESTful API

---
+++ routes/api/posts.js
@@ -0,0 +1,14 @@
+/**
+ * A forum area where we can add posts and like, comment
+ */
+const express = require('express');
+const router = express.Router();
+
+/**
+ * @route GET api/posts
+ * @desc Test route
+ * @access Public
+ */
+router.get('/', (req, res) => res.send('Posts route'));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const connectDB = require('./db');
+const connectDB = require('./config/db');
 
 const app = express();
 
@@ -7,6 +7,12 @@ const app = express();
 connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/users'));
 
 // listen to environment variable called port, default 5000
 const PORT = process.env.PORT || 5000;
