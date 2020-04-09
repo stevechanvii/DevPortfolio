9c0476f0b2711f05b9a9167f47880b97a24b2fa8
Steve Chan <dche0007@student.monash.edu>
2020-04-09 18:37:35 +1000

    setup login and register page

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,20 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () => (
   <Router>
     <Fragment>
       <Navbar />
       <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
     </Fragment>
   </Router>
 );
