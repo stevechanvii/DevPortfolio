58f2a57e2b4837b76e625a074bb4515b4eed546c
Steve Chan <dche0007@student.monash.edu>
2020-04-15 18:44:38 +1000

    bug fixes

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -43,6 +43,7 @@ export default function (state = initialState, action) {
                 ...state,
                 error: payload,
                 loading: false,
+                profile: null,
             };
         case CLEAR_PROFILE:
             return {
